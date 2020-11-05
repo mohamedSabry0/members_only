2d0e0f0b1545f05620c14edd40d3aae72a0e1073
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-11-05 14:25:03 +0200

    fix linter errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,14 @@
 module PostsHelper
-    def user_info(signed_in, post)
-        return {name: post.user.name,
-                username: post.user.username} if signed_in
-        return {name: "anonymous",
-                username: "anonymous"}
+  def user_info(signed_in, post)
+    if signed_in
+      return { name: post.user.name,
+               username: post.user.username }
     end
-    
-    def user_img(signed_in)
-        "https://bulma.io/images/placeholders/64x64.png" if signed_in
-    end
+    { name: 'anonymous',
+      username: 'anonymous' }
+  end
+
+  def user_img(signed_in)
+    'https://bulma.io/images/placeholders/64x64.png' if signed_in
+  end
 end
